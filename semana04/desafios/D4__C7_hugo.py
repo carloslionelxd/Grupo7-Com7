108d15c3fd2f0c58759609aabde10b972b3da1da
carloslionelxd <carlos.lionel.barboza@gmail.com>
2023-05-18 22:40:16 -0300

    Agregamos el trabajo de Hugo

---
+++ semana04/desafios/D4__C7_hugo.py
@@ -0,0 +1,164 @@
+def seleccionar_inmuebles(inmuebles, año):
+    seleccion = []
+    for inmueble in inmuebles:
+        if inmueble['estado'] == 'Disponible' and inmueble['año'] == año:
+            seleccion.append(inmueble)
+    return seleccion
+
+def agregar_inmueble(inmuebles):
+    año = int(input('Introduce el año del inmueble: '))
+    if año < 2000:
+        print('El año debe ser mayor o igual a 2000')
+        return
+    metros = int(input('Introduce los metros cuadrados del inmueble: '))
+    if metros < 60:
+        print('Los metros cuadrados deben ser mayores o iguales a 60')
+        return
+    habitaciones = int(input('Introduce el número de habitaciones del inmueble: '))
+    if habitaciones < 2:
+        print('El número de habitaciones debe ser mayor o igual a 2')
+        return
+    garaje = input('¿Tiene garaje el inmueble? (s/n): ').lower() == 's'
+    zona = input('Introduce la zona del inmueble (A/B/C): ')
+    if zona not in ['A', 'B', 'C']:
+        print('La zona debe ser A, B o C')
+        return
+    estado = input('Introduce el estado del inmueble (Disponible/Reservado/Vendido): ')
+    if estado not in ['Disponible', 'Reservado', 'Vendido']:
+        print('El estado debe ser Disponible, Reservado o Vendido')
+        return
+    inmueble = {'año': año, 'metros': metros, 'habitaciones': habitaciones, 'garaje': garaje, 'zona': zona, 'estado': estado}
+    inmuebles.append(inmueble)
+
+def editar_inmueble(inmuebles):
+    indice = int(input('Introduce el índice del inmueble que quieres editar: '))
+    if 0 <= indice < len(inmuebles):
+        año = int(input('Introduce el nuevo año del inmueble: '))
+        if año < 2000:
+            print('El año debe ser mayor o igual a 2000')
+            return
+        metros = int(input('Introduce los nuevos metros cuadrados del inmueble: '))
+        if metros < 60:
+            print('Los metros cuadrados deben ser mayores o iguales a 60')
+            return
+        habitaciones = int(input('Introduce el nuevo número de habitaciones del inmueble: '))
+        if habitaciones < 2:
+            print('El número de habitaciones debe ser mayor o igual a 2')
+            return
+        garaje = input('¿Tiene garaje el inmueble? (s/n): ').lower() == 's'
+        zona = input('Introduce la nueva zona del inmueble (A/B/C): ')
+        if zona not in ['A', 'B', 'C']:
+            print('La zona debe ser A, B o C')
+            return
+        estado = input('Introduce el nuevo estado del inmueble (Disponible/Reservado/Vendido): ')
+        if estado not in ['Disponible', 'Reservado', 'Vendido']:
+            print('El estado debe ser Disponible, Reservado o Vendido')
+            return
+        inmuebles[indice] = {'año': año, 'metros': metros, 'habitaciones': habitaciones, 'garaje': garaje, 'zona': zona, 'estado': estado}
+
+def cambiar_estado(inmuebles):
+    indice = int(input('Introduce el índice del inmueble cuyo estado quieres cambiar: '))
+    if 0 <= indice < len(inmuebles):
+        estado = input('Introduce el nuevo estado del inmueble (Disponible/Reservado/Vendido): ')
+        if estado not in ['Disponible', 'Reservado', 'Vendido']:
+            print('El estado debe ser Disponible, Reservado o Vendido')
+            return
+        inmuebles[indice]['estado'] = estado
+
+def buscar_inmuebles(inmuebles, precio_maximo):
+    seleccion = []
+    año_actual = 2023 
+    for inmueble in inmuebles:
+        metros = inmueble['metros']
+        habitaciones = inmueble['habitaciones']
+        garaje = 1500 if inmueble['garaje'] else 0
+        antiguedad = año_actual - inmueble['año']
+        zona = inmueble['zona']
+        estado = inmueble['estado']
+        if zona == 'A':
+            precio = (metros * 100 + habitaciones * 500 + garaje) * (1 - antiguedad / 100)
+        elif zona == 'B':
+            precio = (metros * 100 + habitaciones * 500 + garaje) * (1 - antiguedad / 100) * 1.5
+        elif zona == 'C':
+            precio = (metros * 100 + habitaciones * 500 + garaje) * (1 - antiguedad / 100) * 2
+        if precio <= precio_maximo and estado in ['Disponible', 'Reservado']:
+            inmueble_con_precio = inmueble.copy()
+            inmueble_con_precio['precio'] = precio
+            seleccion.append(inmueble_con_precio)
+    return seleccion
+
+inmuebles = [{'año': 2010, 'metros': 150, 'habitaciones': 4, 'garaje': True, 'zona': 'C', 'estado': 'Disponible'},
+{'año': 2016, 'metros': 80, 'habitaciones': 2, 'garaje': False, 'zona': 'B', 'estado': 'Reservado'},
+{'año': 2000, 'metros': 180, 'habitaciones': 4, 'garaje': True, 'zona': 'A', 'estado': 'Disponible'},
+{'año': 2015, 'metros': 95, 'habitaciones': 3, 'garaje': True, 'zona': 'B', 'estado': 'Vendido'},
+{'año': 2008, 'metros': 60, 'habitaciones': 2, 'garaje': False, 'zona': 'C', 'estado': 'Disponible'}]
+
+def buscar_propiedades(propiedades, precio):
+    resultado = []
+    for propiedad in propiedades:
+        if propiedad['zona'] not in ['A', 'B', 'C']:
+            continue
+        if propiedad['estado'] not in ['Disponible', 'Reservado', 'Vendido']:
+            continue
+        if propiedad['año'] < 2000:
+            continue
+        if propiedad['metros'] < 60:
+            continue
+        if propiedad['habitaciones'] < 2:
+            continue
+        precio_calculado = calcular_precio(propiedad)
+        if precio_calculado <= precio:
+            propiedad['precio_calculado'] = precio_calculado
+            resultado.append(propiedad)
+    return resultado
+
+def calcular_precio(propiedad):
+    zona = propiedad['zona']
+    tamaño = propiedad['metros']
+    habitaciones = propiedad['habitaciones']
+    garaje = 1 if propiedad['garaje'] else 0
+    año = propiedad['año']
+    antigüedad = 2023 - año
+    precio_base = tamaño * 100 + habitaciones * 500 + garaje * 1500
+    factor_precio = 1 - antigüedad / 100
+    if zona == 'A':
+        precio = precio_base * factor_precio
+    elif zona == 'B':
+        precio = precio_base * factor_precio * 1.5
+    elif zona == 'C':
+        precio = precio_base * factor_precio * 2
+    return precio
+
+propiedades = [{'año': 2010, 'metros': 150, 'habitaciones': 4, 'garaje': True, 'zona': 'C', 'estado': 'Disponible'},
+{'año': 2016, 'metros': 80, 'habitaciones': 2, 'garaje': False, 'zona': 'B', 'estado': 'Reservado'},
+{'año': 2000, 'metros': 180, 'habitaciones': 4, 'garaje': True, 'zona': 'A', 'estado': 'Disponible'},
+{'año': 2015, 'metros': 95, 'habitaciones': 3, 'garaje': True, 'zona': 'B', 'estado': 'Vendido'},
+{'año': 2008, 'metros': 60, 'habitaciones': 2, 'garaje': False, 'zona': 'C', 'estado': 'Disponible'}]
+
+while True:
+    print('1. Seleccionar inmuebles')
+    print('2. Agregar inmueble')
+    print('3. Editar inmueble')
+    print('4. Eliminar inmueble')
+    print('5. Cambiar estado de inmueble') #Seleccione indice 0, 1, 2, ...
+    print('6. Buscar inmuebles por presupuesto')
+    print('7. Salir')
+    opcion = int(input('Elige una opción: '))
+    if opcion == 1:
+        año = int(input('Introduce el año de los inmuebles que quieres seleccionar: '))
+        seleccion = seleccionar_inmuebles(inmuebles, año)
+        print('Inmuebles seleccionados:', seleccion)
+    elif opcion == 2:
+        agregar_inmueble(inmuebles)
+    elif opcion == 3:
+        editar_inmueble(inmuebles)
+    elif opcion == 4:
+        eliminar_inmueble(inmuebles)
+    elif opcion == 5:
+        cambiar_estado(inmuebles)
+    elif opcion == 6:
+        precio_maximo = float(input('Introduce el precio máximo: '))
+        seleccion = buscar_inmuebles(inmuebles, precio_maximo)
+        print('Inmuebles seleccionados:', seleccion)
+    elif opcion == 7:
+        break
